53613fd133af9786eca9125d44cda8c09f374599
jpcoymat <jpcoymat@gmail.com>
2010-08-23 21:42:32 -0500

    modified:   db/migrate/20100824024011_receipt_line_po_relationship.rb

--- db/migrate/20100824024011_receipt_line_po_relationship.rb
+++ db/migrate/20100824024011_receipt_line_po_relationship.rb
@@ -1,5 +1,9 @@
 class ReceiptLinePoRelationship < ActiveRecord::Migration
+
   def self.up
+    remove_column :receipts, :purchase_order_id
+    add_column    :receipt_lines, :purchase_order_object_type, :string, :limit => 100
+    add_column    :receipt_lines, :purchase_order_object_id,  :integer
   end
 
   def self.down
