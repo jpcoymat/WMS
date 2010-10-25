78a13d615e02e6f855fb75a7142600af4051c6e5
Jean Pierre <jpcoymat@gmail.com>
2010-10-24 23:22:00 -0500

    modified:   app/models/location_finder.rb 	modified:   app/models/storage_strategy_line.rb

--- app/models/location_finder.rb
+++ app/models/location_finder.rb
@@ -7,6 +7,23 @@ class LocationFinder
 
   def locate(*args)
     exclude_location = args[:exclude]
+    storage_strategy = get_matching_storage_strategy
+    if storage_strategy
+      storage_location = nil
+      index = 0
+      while  storage_location.nil? && index < storage_strategy.storage_strategy_lines.count
+        storage_strategy_line = storage_strategy.storage_strategy_lines[index]
+        storage_location = find_location(storage_strategy_line.storage_zone, storage_strategy_line.check_criteria)
+        index += 1
+      end
+      if storage_location
+        create_storage_assignments(container, storage_location)
+        
+      end
+    end
+  end
+
+  def create_storage_assignments
     
   end
 
@@ -29,17 +46,13 @@ class LocationFinder
 	  @storage_strategy
   end
 
-  def evaluate_locations(storage_zone, *args)
-    exclude_location = Location.find(args[:exclude_location])
-  end
-
   def get_next_container
     @location_finder_queue = LocationFinderQueue.new
     self.container = @location_finder_queue.get_next_container
     self.warehouse = self.container.warehouse
   end
 
-  def find_location(storage_zone)
+  def find_location(storage_zone, *check_criteria)
     @storage_location = nil
     locations = Location.where(:warehouse_id => @warehouse.id, :storage_zone =>storage_zone.id, :available_for_storage => true).order(:storage_travel_sequence).all
     index = 0
--- app/models/storage_strategy_line.rb
+++ app/models/storage_strategy_line.rb
@@ -2,7 +2,14 @@ class StorageStrategyLine < ActiveRecord::Base
   
   belongs_to    :storage_strategy
   belongs_to    :storage_zone
-  validates	:order_sequence, :storage_zone_id, :storage_strategy_id, :presence => true
+  validates	    :order_sequence, :storage_zone_id, :storage_strategy_id, :presence => true
   validates   	:order_sequence, :uniqueness => true
+
+  def check_criteria 
+    @check_criteria = self.attributes.clone
+    @check_criteria.delete_if  {|k,v| !(k.include?("check"))}
+    @check_criteria
+  end
+
 
 end
