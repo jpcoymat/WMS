a3b70bb88864c9d45b435bf5961e98432ebab1e0
jeanpierre <jpcoymat@manh.com>
2010-11-06 21:57:11 -0400

    modified:   app/models/assignment_detail.rb 	modified:   app/models/location.rb 	modified:   app/models/storage_assignment.rb

--- app/models/assignment_detail.rb
+++ app/models/assignment_detail.rb
@@ -1,2 +1,32 @@
 class AssignmentDetail < ActiveRecord::Base
+
+	belongs_to :assignment
+	validates :assignment_id, :presence => true
+
+	def from_location
+		@from_location = Location.first(self.from_location_id)
+	end
+	
+	def to_location
+		@to_location = Location.first(self.to_location_id)
+	end
+
+	def expected_product
+		@expected_product = Product.first(self.expected_product_id)
+	end
+	
+	def actual_product
+		@actual_product = Product.first(self.actual_product_id)
+	end
+	
+	def from_container
+		@from_container = Container.first(self.from_container_id)
+	end
+	
+	def to_container
+		@to_container = Container.first(self.to_container_id)
+	end
+	
+	
+
 end
--- app/models/storage_assignment.rb
+++ app/models/storage_assignment.rb
@@ -1,4 +1,8 @@
 class StorageAssignment << Assignment
 
-
+	validates :to_location_id, :from_location_id, :from_container_id, :presence => true
+	
+	
+	
+	
 end
