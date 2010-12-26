b8f30f5c4d81477224eb69048eaf833d44a575e4
Jean Pierre <jpcoymat@gmail.com>
2010-12-25 23:42:40 -0500

    modified:   app/models/customer_type.rb

--- app/models/customer_type.rb
+++ app/models/customer_type.rb
@@ -1,10 +1,10 @@
 class CustomerType < ActiveRecord::Base
 
-  belongs_to    :company
-  has_many      :customers
-  has_many      :allocation_strategy_rules
-  validates   	:company_id, :code, :presence => true
-  validates 	:code, :company_id, :uniqueness => true
+  belongs_to                :company
+  has_many                  :customers
+  has_many                  :allocation_strategy_rules
+  validates   	            :company_id, :code, :presence => true
+  validates_uniqueness_of   :code, :scope => :company_id
 
   def deleteable?
     self.customers.empty? and self.allocation_strategy_rules.empty?    
