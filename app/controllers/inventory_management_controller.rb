23c6b4afb4aa40acf99cdbd0469807b8f6c1acd3
Jean Pierre <jpcoymat@gmail.com>
2011-04-02 12:12:22 -0500

    modified:   Gemfile.lock 	modified:   app/controllers/inventory_management_controller.rb

--- app/controllers/inventory_management_controller.rb
+++ app/controllers/inventory_management_controller.rb
@@ -4,13 +4,13 @@ class InventoryManagementController < ApplicationController
   end
 
   def containers
-	@container_location_types = Container.location_types
-	@open_shipments = User.find(session[:user_id]).warehouse.open_shipments
-	if request.post?
-		container_criteria = params[:container].clone
-		container_criteria.delete_if {|k,v| v.is_blank? }
-		@containers = Container.where(container_criteria).all 
-	end
+	  @container_location_types = Container.location_types
+	  @open_shipments = User.find(session[:user_id]).warehouse.open_shipments
+	  if request.post?
+		  container_criteria = params[:container].clone
+		  container_criteria.delete_if {|k,v| v.is_blank? }
+		  @containers = Container.where(container_criteria).all 
+	  end
   end
 
   def inventory_updates
