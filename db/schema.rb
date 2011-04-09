8eb33711af9db7e0d62b7b72be62cef05203c996
Jean Pierre <jpcoymat@gmail.com>
2011-04-09 18:39:50 -0500

    modified:   login_controller.rb 	modified:   ../../db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110311231506) do
+ActiveRecord::Schema.define(:version => 20110404024618) do
 
   create_table "allocation_strategies", :force => true do |t|
     t.string   "name",         :limit => 25, :null => false
@@ -292,6 +292,7 @@ ActiveRecord::Schema.define(:version => 20110311231506) do
     t.datetime "updated_at"
     t.integer  "company_id"
     t.string   "state",            :limit => 50, :null => false
+    t.integer  "warehouse_id",                   :null => false
   end
 
   create_table "product_categories", :force => true do |t|
