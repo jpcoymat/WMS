5589b10bbd71cb75a303b7b3748890562cc0a6fe
Jean Pierre Coymat <jpcoymat@gmail.com>
2011-06-19 23:24:02 -0500

    modified:   app/controllers/javascripts_controller.rb 	deleted:    app/controllers/receiving_controller.rb 	modified:   app/models/purchase_order_line.rb 	modified:   app/models/receipt_line.rb 	modified:   app/views/admin/warehouses/new.html.erb 	modified:   app/views/main/index.html.erb 	deleted:    app/views/receiving/_purchase_order.html.erb 	deleted:    app/views/receiving/_purchase_order_line.html.erb 	deleted:    app/views/receiving/_receipt.html.erb 	deleted:    app/views/receiving/_receipt_line.html.erb 	deleted:    app/views/receiving/edit_purchase_order.html.erb 	deleted:    app/views/receiving/edit_purchase_order_line.html.erb 	deleted:    app/views/receiving/index.html.erb 	deleted:    app/views/receiving/new_purchase_order.html.erb 	deleted:    app/views/receiving/new_receipt.html.erb 	deleted:    app/views/receiving/purchase_order_lines.html.erb 	deleted:    app/views/receiving/purchase_orders.html.erb 	deleted:    app/views/receiving/receipt_lines.html.erb 	deleted:    app/views/receiving/receipts.html.erb 	deleted:    app/views/receiving/view_purchase_order.html.erb 	modified:   config/application.rb 	modified:   config/routes.rb 	modified:   public/stylesheets/mystyle.css

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -3,6 +3,11 @@ class JavascriptsController < ApplicationController
   def dynamic_product_subcategories
     @product_subcategories = User.find(session[:user_id]).company.product_subcategories
   end
+  
+  def dynamic_po_lines
+    @purchase_orders = User.find(session[:user_id]).company.purchase_orders
+    
+  end
 
 
 end
