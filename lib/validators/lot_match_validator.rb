f07ae9c25ebb8b9d9cea357c5962b20d64909416
Jean Pierre Coymat <jpcoymat@gmail.com>
2011-08-05 08:59:40 -0500

    modified:   container_content.rb 	modified:   ../../lib/validators/lot_match_validator.rb 	modified:   ../../lib/validators/product_match_validator.rb

--- lib/validators/lot_match_validator.rb
+++ lib/validators/lot_match_validator.rb
@@ -2,12 +2,10 @@ class LotMatchValidator < ActiveModel::EachValidator
   
   def validate_each(record, attribute, value)
     unless value.nil?
-      order_line_product = record.product
+      product = record.product
       lot = Lot.find(value)
-      if lot
-        unless order_line_product.lots.include?(lot)
-          record.errors[attribute] << "Lot does not exist for product."
-        end
+      unless product.lots.include?(lot)
+        record.errors[attribute] << "Lot does not exist for product."
       end
     end
   end
