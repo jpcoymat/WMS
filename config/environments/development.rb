f46da3277a29f4cebee7befb0abeaf9905ace803
Jean Pierre Coymat <jpcoymat@gmail.com>
2013-08-03 18:51:00 -0500

    tweaks to rails 4 upgrade

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,6 @@ Wms::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -29,4 +27,5 @@ Wms::Application.configure do
   # config.assets.debug = true
   config.active_record.mass_assignment_sanitizer = :strict
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.eager_load = false
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Wms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.eager_load = true
+  
 end
