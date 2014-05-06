df8cb2bca604e48fe9a83370aab931a3b6d28417
Jean Pierre Coymat <jpcoymat@gmail.com>
2014-05-05 21:14:16 -0700

    changes

--- app/controllers/api/v1/purchase_orders_controller.rb
+++ app/controllers/api/v1/purchase_orders_controller.rb
@@ -5,4 +5,21 @@ class Api::V1::PurchaseOrdersController < ApplicationController
     po = PurchaseOrder.find_by_purchase_order_number(params[:po_number])
     respond_with(po, :status => po.blank? ? 404 : 200)
   end
+  
+  def search_by_supplier
+    supplier = Supplier.where(name: params[:supplier_name]).first
+    if supplier
+      respond_with(supplier.purchase_orders.all)
+    else
+      respond_with(supplier, status: 404)
+    end
+  end
+  
+  def list
+    respond_with(PurchaseOrder.all, status: 200)
+  end
+
+  
+  
+  
 end
